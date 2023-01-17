48e536aab19354b39853f2a8317facffb088a329
Christian Mejia <chrismejia10@gmail.com>
2023-01-16 19:20:42 -0500

    feat(realtime): create RealtimeMessages component using new Message type

---
+++ components/RealtimeMessages.tsx
@@ -0,0 +1,11 @@
+import type { Database } from "db_types";
+
+type Message = Database["public"]["Tables"]["messages"]["Row"];
+
+export default function RealtimeMessages({
+  serverMessages,
+}: {
+  serverMessages: Message[];
+}) {
+  return <pre>{JSON.stringify(serverMessages, null, 2)}</pre>;
+}
